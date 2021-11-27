58404303cf4c57653ad174df2a69e013c01b4932
Devin Dai <wdai@ualberta.ca>
2021-11-27 14:27:36 -0700

    added grid with images

--- src/Components/grid.js
+++ src/Components/grid.js
@@ -3,16 +3,34 @@ import React from "react";
 class Grid extends React.Component{
     render(){
         return(
-            <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-center p-10">
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">1</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">2</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">3</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">4</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">5</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">6</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">7</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">8</div>
-                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100">9</div>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 justify-center p-5" >
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://pbs.twimg.com/media/Do73TSDU4AEgQLr.jpg" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://wallpaperaccess.com/full/1322048.jpg" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://i.pinimg.com/originals/01/7e/52/017e52ecd2b2bdd776566b0574639055.png" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://itadakimasuanime.files.wordpress.com/2013/06/naan-shirokuma-cafe-17-06.png" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://cloudedanime.files.wordpress.com/2017/01/dragon-ball-super-73-01-ramen.jpg" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://itadakimasuanime.files.wordpress.com/2016/12/0curry-haifuri-02.png" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://img.buzzfeed.com/buzzfeed-static/static/2017-11/27/12/asset/buzzfeed-prod-fastlane-03/sub-buzz-18463-1511805244-17.jpg" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://i.pinimg.com/originals/68/be/a9/68bea902596e5459ac896b169dca5591.png" alt="1" />
+                </div>
+                <div className="flex justify-center text-4xl border-2 border-gray-300 rounded-xl p-6 bg-gray-100 cursor-pointer hover:bg-gray-300">
+                    <img src="https://64.media.tumblr.com/ecf848e6c779002bd38d83ef620f6406/tumblr_pb8kxraKnu1v656bko1_1280.png" alt="1" />
+                </div>
             </div>
         )
     }
