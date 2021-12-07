b7ffcff5475a9574e2063c238b80248622841029
Devin Dai <wdai@ualberta.ca>
2021-12-07 02:12:13 -0700

    Modal

--- src/Components/mainContent.js
+++ src/Components/mainContent.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const MainContent = () =>{
     return(
-        <div className="bg-gradient-to-l md:bg-gradient-to-r flex justify-center items-center">
-            <img src="https://cdnb.artstation.com/p/assets/images/images/035/798/583/large/gabriel-peicic-5shot.jpg?1615925077" alt="1" />
+        <div className="bg-gradient-to-l md:bg-gradient-to-r flex justify-center items-center ">
+            <img className="h-screen" src="https://cdnb.artstation.com/p/assets/images/images/035/798/583/large/gabriel-peicic-5shot.jpg?1615925077" alt="1" />
         </div>
     )
 }
