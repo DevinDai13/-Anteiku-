b1d2a053fcf48d4b618a8f61e340f4eed1c18e53
Devin Dai <wdai@ualberta.ca>
2021-12-17 20:50:32 -0700

    background image and theme updates

--- src/Components/characters.js
+++ src/Components/characters.js
@@ -3,17 +3,38 @@ import React from "react";
 const Characters = () =>{
     return(
         <>
-            <div className="flex justify-center items-center mt-20 ml-20 mr-20 bg-gray-700 rounded-tl-Large">
-                <img className="sm:w-30 h-30 md:w-40 h-40 lg:w-64 h-64 rounded-full flex" src="https://numeralpaint.com/wp-content/uploads/2020/07/kaneki-tokyo-ghoul-adult-paint-by-numbers.jpg" alt="1" />
-                <p className="text-white pl-10 flex">Kaneki now lives under the new identity of Haise Sasaki and has been taken into the care of the CCG</p>
+            <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div class="md:flex">
+                    <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://numeralpaint.com/wp-content/uploads/2020/07/kaneki-tokyo-ghoul-adult-paint-by-numbers.jpg" alt="Man looking at item at a store"></img>
+                    </div>
+                    <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                    <p class="mt-2 text-gray-500">Kaneki now lives under the new identity of Haise Sasaki and has been taken into the care of the CCG</p>
+                    </div>
+                </div>
             </div>
-            <div className="flex justify-center items-center ml-20 mr-20 bg-gray-700">
-                <p className="text-white pr-10 flex">Touka Kirishima is the deuteragonist and a ghoul and waitress at Anteiku and one of the main characters of the Tokyo Ghoul franchise</p>
-                <img className="sm:w-30 h-30 md:w-40 h-40 lg:w-64 h-64 rounded-full flex" src="https://images6.fanpop.com/image/photos/39400000/Touka-kirishima-touka-kirishima-39481726-1280-720.png" alt="1" />
+            <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div class="md:flex">
+                    <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://images6.fanpop.com/image/photos/39400000/Touka-kirishima-touka-kirishima-39481726-1280-720.png" alt="Man looking at item at a store"></img>
+                    </div>
+                    <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                    <p class="mt-2 text-gray-500">Touka Kirishima is the deuteragonist and a ghoul and waitress at Anteiku and one of the main characters of the Tokyo Ghoul franchise</p>
+                    </div>
+                </div>
             </div>
-            <div className="flex justify-center items-center ml-20 mr-20 ml-20 mb-20 bg-gray-700 rounded-br-Large">
-                <img className="sm:w-30 h-30 md:w-40 h-40 lg:w-64 h-64 rounded-full flex" src="https://static3.cbrimages.com/wordpress/wp-content/uploads/2020/06/Hide-in-Tokyo-Ghoul-re.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5" alt="1" />
-                <p className="text-white pl-10 flex">Nagachika Hideyoshi is a normal human and the best friend of Ken Kaneki</p>
+            <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div class="md:flex">
+                    <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://static3.cbrimages.com/wordpress/wp-content/uploads/2020/06/Hide-in-Tokyo-Ghoul-re.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5" alt="Man looking at item at a store"></img>
+                    </div>
+                    <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                    <p class="mt-2 text-gray-500">Nagachika Hideyoshi is a normal human and the best friend of Ken Kaneki</p>
+                    </div>
+                </div>
             </div>
         </>
     )
