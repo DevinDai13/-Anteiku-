8c0e815b212136b995110b5f3acac445cd55d521
Devin Dai <wdai@ualberta.ca>
2022-01-01 10:41:37 -0700

    updated footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -10,19 +10,22 @@ const Footer = () =>{
                     <div class="sm:flex sm:mt-8">
                         <div class="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
                             <div class="flex flex-col">
-                                <span class="font-bold text-orange-500 uppercase mb-2">Footer header 1</span>
-                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">link 1</a></span>
-                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">link 1</a></span>
+                                <span class="font-bold text-orange-500 uppercase mb-2">ABOUT US</span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Store Locator</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Gift Cards</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Careers</a></span>
                             </div>
                             <div class="flex flex-col">
-                                <span class="font-bold text-orange-500 uppercase mt-4 md:mt-0 mb-2">Footer header 2</span>
-                                <span class="my-2"><a href="#0" class="text-white text-md hover:text-blue-500">link 1</a></span>
-                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">link 1</a></span>
+                                <span class="font-bold text-orange-500 uppercase mt-4 md:mt-0 mb-2">VISIT US</span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Los Angeles</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Las Vegas</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Sydney</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Shanghai</a></span>
                             </div>
                             <div class="flex flex-col">
-                                <span class="font-bold text-orange-500 uppercase mt-4 md:mt-0 mb-2">Footer header 3</span>
-                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">link 1</a></span>
-                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">link 1</a></span>
+                                <span class="font-bold text-orange-500 uppercase mt-4 md:mt-0 mb-2">LEGAL</span>
+                                <span class="my-2"><a href="#0" class="text-white text-md hover:text-blue-500">Privacy Policy</a></span>
+                                <span class="my-2"><a href="#0" class="text-white  text-md hover:text-blue-500">Trademark</a></span>
                             </div>
                         </div>
                     </div>
