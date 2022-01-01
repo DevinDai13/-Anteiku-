3fa09cf1b2fbe473bcaedccfc4f53e7e4782e1e2
Devin Dai <wdai@ualberta.ca>
2022-01-01 11:45:18 -0700

    updated scroll team

--- src/Pages/team.js
+++ src/Pages/team.js
@@ -28,7 +28,7 @@ function Team () {
         </div>
 
         <div style={background2style} class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-            <div className="rounded overflow-hidden shadow-lg w3-animate-left">
+            <div className="rounded overflow-auto shadow-lg w3-animate-left">
                 <img className="w-full" src={"https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002566/img/basic/a0002566_main.jpg?20201027184855&q=80&rw=750&rh=536"} alt="Mountain" />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">Member 1</div>
@@ -39,7 +39,7 @@ function Team () {
             <div className="px-6 pt-4 pb-2">
             </div>
             </div>
-            <div className="rounded overflow-hidden shadow-lg w3-animate-bottom">
+            <div className="rounded overflow-auto shadow-lg w3-animate-bottom">
                 <img className="w-full" src="https://luxurylaunches.com/wp-content/uploads/2019/09/Businessman-fashion.jpeg" alt="River" />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">Member 2</div>
@@ -50,7 +50,7 @@ function Team () {
             <div className="px-6 pt-4 pb-2">
             </div>
             </div>
-            <div className="rounded overflow-hidden shadow-lg w3-animate-right">
+            <div className="rounded overflow-auto shadow-lg w3-animate-right">
                 <img className="w-full" src="https://i0.wp.com/www.tycoonstory.com/wp-content/uploads/2016/12/successful-business-tycoon-tycoonstory.png?fit=700%2C400&ssl=1" alt="Forest" />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">Member 3</div>
