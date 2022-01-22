fb8b71c7ebbc9d055a70b6057aa96a9b91119e16
Devin Dai <wdai@ualberta.ca>
2022-01-21 19:45:41 -0700

    mobile nav updated

--- src/Components/navbarV2.js
+++ src/Components/navbarV2.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import {GiHamburgerMenu} from "react-icons/gi";
 import {VscChromeClose} from "react-icons/vsc";
+import navbg from "../Images2/navbg.jpg";
 import {
     HOME_URL,
     MENU_URL,
@@ -74,9 +75,47 @@ const ResponsiveNav = styled.div`
   right: -100vw;
   top: 0;
   z-index: 10;
-  background-color: white;
+  background-image: linear-gradient(315deg, #f9d29d 0%, #ffd8cb 74%);
   height: 100vh;
   width: ${({ state }) => (state ? "60%" : "0%")};
+  transition: 0.3s ease-in-out;
+  display: flex;
+  opacity: 0;
+  visibility: hidden;
+  ul {
+    list-style-type: none;
+    width: 100%;
+    margin-top: 3rem;
+    li {
+      width: 100%;
+      margin: 1rem 0;
+      margin-left: 2rem;
+      a {
+        text-decoration: none;
+        color: tomato;
+        font-size: 1.2rem;
+        transition: 0.1s ease-in-out;
+        &:hover {
+          color: black;
+        }
+      }
+      &:first-of-type {
+        a {
+          color: #fc4958;
+          font-weight: 900;
+        }
+      }
+    }
+  }
+`;
+
+const ResponsiveNavLeft = styled.div`
+  position: fixed;
+  left: -100vw;
+  top: 0;
+  z-index: 10;
+  height: 100vh;
+  width: ${({ state }) => (state ? "40%" : "0%")};
   transition: 0.3s ease-in-out;
   display: flex;
   opacity: 0;
@@ -115,12 +154,24 @@ function NavbarV2() {
 
     const html = document.querySelector("html");
     html.addEventListener("click", () => setNavbarState(false));
+
+    useEffect(() => {
+      var prevScrollpos = window.pageYOffset;
+      window.onscroll = function(){
+      var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos)
+          document.getElementById("navbar").style.top = "0";
+        else 
+          document.getElementById("navbar").style.top = "-50px";
+        prevScrollpos = currentScrollPos;
+      }
+    })
     
     return (
       <div>
-        <Nav >
+        <Nav id="navbar" className="h-20 text-xl">
           <div className="brand">
-            <h3>SUNSET BOULEVARD</h3>
+            <h3 className="text-2xl">SUNSET BOULEVARD</h3>
             <div className="toggle">
               {navbarState ? (
                 <VscChromeClose onClick={() => setNavbarState(false)} />
@@ -150,6 +201,10 @@ function NavbarV2() {
             <li><a href={CONTACT_URL} onClick={() => setNavbarState(false)}>Contact</a></li>
           </ul>
         </ResponsiveNav>
+
+        <ResponsiveNavLeft state={navbarState} className={navbarState ? "showLeft" : ""}>
+          <img src={navbg} alt="1" />
+        </ResponsiveNavLeft>
       </div>
     );
   }
